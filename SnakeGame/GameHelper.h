c08a7731ea2e0f7683be808778239b1662193e9e
Виталий Шовкун <vetalshovkun@gmail.com>
2025-05-10 18:42:31 +0300

    Add GameHelper

--- SnakeGame/GameHelper.h
+++ SnakeGame/GameHelper.h
@@ -1,1 +1,40 @@
 #pragma once
+#include<iostream>
+#include<windows.h>
+#include<stdlib.h>
+
+const int x_max = 100;
+const int y_max = 30;
+
+enum Rotation
+{
+	left,
+	right,
+	up,
+	down
+};
+
+struct Position
+{
+	int x;
+	int y;
+};
+
+class GameHelper
+{
+	public:
+		static void GoToXY(int x, int y)
+		{
+			COORD pos = { x, y };
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			SetConsoleCursorPosition(handle, pos);
+		}
+		static void CursorVisible(bool visible)
+		{
+			HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+			CONSOLE_CURSOR_INFO cursorInfo;
+			GetConsoleCursorInfo(handle, &cursorInfo);
+			cursorInfo.bVisible = visible;
+			SetConsoleCursorInfo(handle, &cursorInfo);
+		}
+};
